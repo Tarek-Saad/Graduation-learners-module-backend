d7e0ec11b014841e1edd2698d22cec20b2dce13b
Tarek <saadfouad1976t@gmail.com>
2025-03-30 07:33:44 +0200

    send data remote to atlas

--- conn/connection.js
+++ conn/connection.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connection = async() => {
+    const env = process.env.NODE_ENV || 'development';
+
+    const dbURI = env === 'production' ?
+        process.env.MONGO_URI_PROD :
+        process.env.MONGO_URI_LOCAL;
+
     try {
-        await mongoose.connect('mongodb://localhost:27017/ls-backend');
-        console.log('Connected to MongoDB');
+        await mongoose.connect(dbURI);
+        console.log(`Connected to MongoDB (${env})`);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // exit app if DB fails to connect
     }
 };
 
