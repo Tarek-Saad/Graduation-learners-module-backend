8121648c9140cccc8002d35f81b2ff9a18fbf5a4
Tarek <saadfouad1976t@gmail.com>
2025-03-30 08:02:21 +0200

    remove port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api', learnerRoutes);
 app.use('/questions', questionsRouter);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 // });
