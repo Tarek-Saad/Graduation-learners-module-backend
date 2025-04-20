4b2ffafca0201f95a5579ed109b858b0f217bc4c
Tarek <saadfouad1976t@gmail.com>
2025-04-20 23:18:48 +0200

    ...

--- api/index.js
+++ api/index.js
@@ -11,9 +11,31 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+
 app.use(morgan('dev'));
 app.use(express.json());
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://codengo.vercel.app', '*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if needed for cookies
+};
+
+app.use(cors(corsOptions));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+} else {
+    app.use(morgan('combined'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-ten-jet-74.vercel.app');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
